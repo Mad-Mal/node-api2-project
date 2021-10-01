87114701a516afdc3b7f8a26a08c6c5fa6837b8d
Mallory <mallorydoescoding@gmail.com>
2021-09-30 20:18:45 -0500

    index.js setup.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,7 @@
-// implement your posts router here
+//Imports
+const Posts = require("./posts-model.js");
+const express = require("express");
+const router = express.Router();
+
+// Posts endpoints
+
