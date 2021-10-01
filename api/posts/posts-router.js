2bb50557cb1c9520a7fcdf24ea7999b66c03baf6
Mallory <mallorydoescoding@gmail.com>
2021-09-30 20:27:48 -0500

    I have the posts-router component pre-organized to make endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,3 +5,20 @@ const router = express.Router();
 
 // Posts endpoints
 
+// [Get], returns array of all the post objects
+
+
+// [Get], returns post object with the specified ID
+
+
+// [Post], creates post with info from req.body and returns new post
+
+
+// [Put], updates post with the specified ID using data from req.body and returns the modified post
+
+
+// [Delete], returns array of all the comment objects associated with the specified post ID
+
+
+
+module.exports = router;
