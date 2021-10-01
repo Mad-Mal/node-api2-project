b020a071d6da22e3aef84b50d523ba1d9525686f
Mallory <mallorydoescoding@gmail.com>
2021-09-30 21:18:19 -0500

    Second endpoint created.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -21,6 +21,23 @@ router.get("/", (req,res) => {
 
 // [Get], returns post object with the specified ID
 
+router.get("/:id", (req,res) => {
+    Posts.findById(req.params.id)
+        .then(posts => {
+            if (posts) {
+            res.status(200).json(posts);
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            };
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved" 
+            })
+        })
+})
 
 // [Post], creates post with info from req.body and returns new post
 
